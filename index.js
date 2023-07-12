327f3ee225b37a39804aaa66409388a93bdc223d
KyleHS <k15156@yahoo.com>
2023-07-11 20:18:36 -0600

    fixed issue not allowing us to see webpage

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
-const express = require('express'); //require express.js
+const express = require('express');
 const app = express();
 const path = require('path');
+const redditData = require('./data.json');
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views')); //best practice to use this on every file.. allows us to use nodemon from base directory
+app.set('views', path.join(__dirname, '/views'))
 
 app.get('/', (req, res) => {
-    res.render('home.ejs')  //can also just use 'home' since we referenced ejs in app.set
-});
+    res.render('home')
+})
 
 app.get('/cats', (req, res) => {
     const cats = [
-        'Ralsei', 'Susie', 'Kris', 'Winton'
+        'Blue', 'Rocket', 'Monty', 'Stephanie', 'Winston'
     ]
-    res.render('cats', { cats });
+    res.render('cats', { cats })
 })
-
-app.get('/rand', (req, res) => {
-    const num = Math.floor(Math.random() * 10) + 1
-    res.render('random', { rand: num})  //can also just use 'home' since we referenced ejs in app.set
-});
 
 app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params;
-    res.render('subreddit', { subreddit });
+    const data = redditData[subreddit];
+    if (data) {
+        res.render('subreddit', { ...data });
+    } else {
+        res.render('notfound', { subreddit })
+    }
 })
 
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.render('random', { num })
+})
 
-app.listen(3000, () => {  //setup server port to localhost:3000
-    console.log("Listening on port 3000")
-});
+app.listen(3000, () => {
+    console.log("LISTENING ON PORT 3000")
+})
